fbbd8fd896422d3048335c03066dfaf3ec23bc12
Ivan_Trifonov <ivan14trifonov@gmail.com>
2023-06-20 22:26:12 +0300

    v5

--- src/blocks/coordinates/index.tsx
+++ src/blocks/coordinates/index.tsx
@@ -13,7 +13,9 @@ export const Coordinates = () => {
 			<div className="place">
 				<h2 className="text">📍 Место проведения</h2>
 				<a
-					href="https://yandex.ru/maps/?ll=38.384608%2C53.866330&mode=search&sll=38.421775%2C53.916805&text=53.916805%2C38.421775&z=10"
+					// href="https://yandex.ru/maps/?ll=38.384608%2C53.866330&mode=search&sll=38.421775%2C53.916805&text=53.916805%2C38.421775&z=10"
+					// href="https://yandex.ru/maps/?pt=38.421775,53.916805&z=18&l=map"
+					href="https://yandex.ru/maps/-/CCUD4RvbtA"
 					target="_blank"
 					className="link"
 					rel="noreferrer">
@@ -21,25 +23,35 @@ export const Coordinates = () => {
 				</a>
 			</div>
 			<div className="map1">
-				<a className="map2" href="https://yandex.ru/maps?utm_medium=mapframe&utm_source=maps" rel="noreferrer">
+{/*				<a className="map2" href="https://yandex.ru/maps?utm_medium=mapframe&utm_source=maps" rel="noreferrer">
 						Яндекс Карты
-				</a>
-      			<a
+				</a>*/}
+{/*      			<a
       			    className="map3"
       			    rel="noreferrer"
       				href="https://yandex.ru/maps/?ll=38.384608%2C53.866330&mode=search&sll=38.421775%2C53.916805&text=53.916805%2C38.421775&z=10">
       				Место проведения лагеря — Яндекс Карты
-      			</a>
-      			<iframe
+      			</a>*/}
+				<iframe
+				className="map"
+				title="Карта"
+				rel="noreferrer"
+				src="https://yandex.ru/map-widget/v1/?um=constructor%3A0527761ad143fd73b15e27a3e83db3890f6f61933be19f3b5c8a6fc116c7f5f0&amp;source=constructor"
+				width="500"
+				height="400"
+				frameborder="0"
+				></iframe>
+{/*      			<iframe
       			    className="map"
 				title="Карта"
 				rel="noreferrer"
-      				src="https://yandex.ru/map-widget/v1/-/CCUDETDrdD"
+				    src="https://yandex.ru/maps/-/CCUD4RvbtA"
+      				//src="https://yandex.ru/map-widget/v1/-/CCUDETDrdD"
 
       				// frameborder="0"
       				// allowfullscreen="true"
       				>
-      			</iframe>
+      			</iframe>*/}
       		</div>
 		</div>
 		)
