98da75353609d7a23e5280645dc7701cb276934a
Ivan_Trifonov <ivan14trifonov@gmail.com>
2023-06-20 23:04:58 +0300

    v9

--- src/blocks/description/index.tsx
+++ src/blocks/description/index.tsx
@@ -44,7 +44,7 @@ export const Description = () => {
 						</ul>
 					</div>
 					<div className="description__text">
-						<p><b>Ответственный за лагерь: Шкаровский Георгий, <phone>+7 920 275 1508</phone>.</b></p>
+						<p><b>Ответственный за лагерь: Шкаровский Георгий, +7 920 275 1508.</b></p>
 					</div>
 					<div className="description__price">
 						<p><b>С собой необходимо привезти 2300 ₽.</b></p>
