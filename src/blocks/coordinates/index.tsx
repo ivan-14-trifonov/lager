d3eae81be353f274cab9e22b0f03a97cf8fcd0cd
Ivan Trifonov <ivan14trifonov@gmail.com>
2024-06-22 22:09:59 +0300

    2024 1.4

--- src/blocks/coordinates/index.tsx
+++ src/blocks/coordinates/index.tsx
@@ -22,37 +22,14 @@ export const Coordinates = () => {
 				Место в Яндекс Картах ➡️
 				</a>
 			</div>
-			<div className="map1">
-{/*				<a className="map2" href="https://yandex.ru/maps?utm_medium=mapframe&utm_source=maps" rel="noreferrer">
-						Яндекс Карты
-				</a>*/}
-{/*      			<a
-      			    className="map3"
-      			    rel="noreferrer"
-      				href="https://yandex.ru/maps/?ll=38.384608%2C53.866330&mode=search&sll=38.421775%2C53.916805&text=53.916805%2C38.421775&z=10">
-      				Место проведения лагеря — Яндекс Карты
-      			</a>*/}
+{/*			<div className="map1">
 				<iframe
 				className="map"
 				title="Карта"
 				rel="noreferrer"
 				src="https://yandex.ru/map-widget/v1/?um=constructor%3A0527761ad143fd73b15e27a3e83db3890f6f61933be19f3b5c8a6fc116c7f5f0&amp;source=constructor"
-				// width="500"
-				// height="400"
-				// frameborder="0"
 				></iframe>
-{/*      			<iframe
-      			    className="map"
-				title="Карта"
-				rel="noreferrer"
-				    src="https://yandex.ru/maps/-/CCUD4RvbtA"
-      				//src="https://yandex.ru/map-widget/v1/-/CCUDETDrdD"
-
-      				// frameborder="0"
-      				// allowfullscreen="true"
-      				>
-      			</iframe>*/}
-      		</div>
+      		</div>*/}
 		</div>
 		)
 }
